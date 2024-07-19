22536de13df8f8c2e5b5a617996ce4a141c926e0
Nicholas Valdez <nvaldez@primehealthservices.com>
2024-07-19 10:27:29 -0500

    chore: cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" className={`${GeistSans.variable}`} data-theme="emerald">
         <body className="flex h-screen min-h-screen flex-col bg-gradient-to-b from-slate-600 to-slate-800 text-xl text-white">
           <header className="flex justify-between pr-5 pt-5 text-lg">
@@ -49,6 +47,7 @@ export default function RootLayout({
           <main className="mt-24 flex flex-1 flex-col items-center sm:mt-72">
             {children}
           </main>
+        </body>
       </html>
     </ClerkProvider>
   );
