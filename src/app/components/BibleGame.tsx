e37e2ac6bee0cbe73153d4221ca28ac4d015ac5a
CalebSullivan35 <CalebSullivan357@gmail.com>
2024-07-22 06:26:50 -0500

    move scoretracking location

--- src/app/components/BibleGame.tsx
+++ src/app/components/BibleGame.tsx
@@ -36,41 +36,45 @@ export const BibleGame = ({ CurrentHighScore }: BibleGameProps) => {
   }, [currentScore]);
 
   return (
-    <div className="h-full">
-      <div className="flex flex-col items-center">
-        <span className="text-4xl sm:text-6xl">{book.name}</span>
-        <div className="mb-6 flex flex-col items-center justify-center">
-          <span className="text-lg sm:text-xl">What comes next?</span>
-          <GameFeedback
-            selectedOption={selectedOption}
+    <>
+      <div className="h-full">
+        <div className="flex flex-col items-center">
+          <span className="text-4xl sm:text-6xl">{book.name}</span>
+          <div className="mb-6 flex flex-col items-center justify-center">
+            <span className="text-lg sm:text-xl">What comes next?</span>
+            <GameFeedback
+              selectedOption={selectedOption}
+              correctBook={correctBook}
+            />
+          </div>
+          <GameOptions
+            options={options}
             correctBook={correctBook}
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+            currentScore={currentScore}
+            setCurrentScore={setCurrentScore}
           />
-        </div>
-        <GameOptions
-          options={options}
-          correctBook={correctBook}
-          selectedOption={selectedOption}
-          setSelectedOption={setSelectedOption}
-          currentScore={currentScore}
-          setCurrentScore={setCurrentScore}
-        />
-        <button
-          className={`sm:text-md ${selectedOption === correctBook ? "btn-primary" : "btn-error"}
+          <button
+            className={`sm:text-md ${selectedOption === correctBook ? "btn-primary" : "btn-error"}
  btn text-base [&:not(:hover)]:btn-outline lg:text-lg xl:text-2xl`}
-          onClick={() => {
-            setBook(getRandomBibleBookName());
-            setSelectedOption(null);
-            newBookButtonCheckForAnswer(selectedOption, setCurrentScore);
-          }}
-        >
-          Click for a new book
-        </button>
+            onClick={() => {
+              setBook(getRandomBibleBookName());
+              setSelectedOption(null);
+              newBookButtonCheckForAnswer(selectedOption, setCurrentScore);
+            }}
+          >
+            Click for a new book
+          </button>
+        </div>
       </div>
-      <div className="mb-10 mt-20 flex flex-col text-center sm:text-3xl">
-        <span>Current Streak</span>
-        <span>{currentScore}</span>
+      <div className="text-md sm: m-10 flex w-full flex-row items-center justify-between px-10 text-base sm:w-fit ">
+        <div className="flex flex-col text-center  sm:mr-24 sm:text-3xl">
+          <span>Current Streak</span>
+          <span>{currentScore}</span>
+        </div>
+        {CurrentHighScore && <HighScore score={CurrentHighScore} />}
       </div>
-      {CurrentHighScore && <HighScore score={CurrentHighScore} />}
-    </div>
+    </>
   );
 };
--- src/app/components/HighScore.tsx
+++ src/app/components/HighScore.tsx
@@ -4,9 +4,9 @@ interface HighScoreProps {
 
 export const HighScore = ({ score }: HighScoreProps) => {
   return (
-    <div className="flex flex-col sm:text-3xl">
-      <span className="text-center sm:text-3xl">Personal High Score</span>
-      <span className="text-center">{score}</span>
+    <div className="flex flex-col text-center sm:text-3xl">
+      <span className="sm:text-3xl">Personal High Score</span>
+      <span>{score}</span>
     </div>
   );
 };
