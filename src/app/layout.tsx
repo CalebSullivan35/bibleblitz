58b3e311575dc5c190ceae32ee54279ff1ea2c0d
CalebSullivan35 <CalebSullivan357@gmail.com>
2024-08-23 21:37:14 -0500

    reactt query install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
 import "~/styles/globals.css";
 import { NavBar } from "./components/NavBar";
+import { ReactQueryCloentProvider } from "./components/ReactQueryClientProvider";
 export const metadata = {
   title: "Bible Blitz",
   description: "A Bible Drill App",
@@ -17,19 +18,21 @@ export default function RootLayout({
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
-      <html
-        lang="en"
-        className={`${GeistSans.variable} bg-slate-800`}
-        data-theme="emerald"
-      >
-        <body className="flex h-screen min-h-screen flex-col  text-xl text-white">
-          <NavBar>
-            <main className="flex flex-1 flex-col items-center">
-              {children}
-            </main>
-          </NavBar>
-        </body>
-      </html>
+      <ReactQueryCloentProvider>
+        <html
+          lang="en"
+          className={`${GeistSans.variable} bg-slate-800`}
+          data-theme="emerald"
+        >
+          <body className="flex h-screen min-h-screen flex-col  text-xl text-white">
+            <NavBar>
+              <main className="flex flex-1 flex-col items-center">
+                {children}
+              </main>
+            </NavBar>
+          </body>
+        </html>
+      </ReactQueryCloentProvider>
     </ClerkProvider>
   );
 }
